63ab5ed2725e7b58d655277fcc3a6af14cbe4cdc
JOHNSTEVE BIRON (XENO) <162690468+Tresehshs@users.noreply.github.com>
2025-03-04 23:29:13 +0800

    Update and rename XenocpmTool (3).py to cpmnukertool.py

--- cpmnukertool.py
+++ cpmnukertool.py
@@ -11,7 +11,7 @@ from rich.style import Style
 import pystyle
 from pystyle import Colors, Colorate
 
-from xenocpm import Tresehshs
+from cpmnuker import Tresehshs
 
 __CHANNEL_USERNAME__ = "XenoMainChannel"
 __GROUP_USERNAME__   = "XenoMainChat"
